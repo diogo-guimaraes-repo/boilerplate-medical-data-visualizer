32e2e70305fc0e99b946ddeb04f73c99ad843097
diogoguimaraesr <diogo.m.guimaraes.92@gmail.com>
2021-01-21 14:42:57 +0000

    implementation finished

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -30,28 +30,27 @@ def draw_cat_plot():
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
+    fig = fig.fig
+
     return fig
 
 
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))] 
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr()
 
     # Generate a mask for the upper triangle
-    mask = None
-
-
+    mask = np.triu(corr)
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(9,9))
 
     # Draw the heatmap with 'sns.heatmap()'
-
-
+    sns.heatmap(round(corr,1), fmt='.1f', linewidths=.1, mask=mask, vmax=.3, center=0.09,square=True, cmap = "rocket",annot = True ,cbar_kws =     {'orientation' : 'vertical'})
 
     # Do not modify the next two lines
     fig.savefig('heatmap.png')
